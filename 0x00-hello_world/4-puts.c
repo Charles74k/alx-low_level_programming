c151c2b70ccda2c836bba25ca798b3dec9212311
root <Charles74k>
2023-02-16 18:48:15 -0800

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -A program that prints some text
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
