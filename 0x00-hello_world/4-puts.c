1e7fb450c251422a83bd0d67f1602a2319a28ef4
root <Charles74k>
2023-02-16 18:51:55 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main -A program that prints some text
  * Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
