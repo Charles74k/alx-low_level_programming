9c5bb5bc025d430dc4078164c36882125f63d285
root <Charles74k>
2023-02-20 18:58:06 -0800

    sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,7 +5,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{char alp[26] = "abcdefghijklmnopqrstuvwzyz";
+{char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
 	for (i = 0; i < 26; i++)
